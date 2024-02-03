bf2c13646de06312a118d94ce640466ce5662bc4
Shuwana <shuwanab@icloud.com>
2024-02-03 04:21:24 -0500

    Created the landing page for the app.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import food from './unsplash_FguE8P699zw.png';
+import background from "./unsplash_FguE8P699zw.jpg";
+import React from 'react';
+import './App.css'; // Assuming you have a CSS file for styling
 
-function App() {
+const App = () => {
+  const styles = {
+
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="left-side">
+        {/* Content for the left side */}
+        <div class="bg-img" style={{ backgroundImage: `url(${background})`,
+          backgroundSize: "cover",
+          objectFit: "fill",
+          height: "100%"}}>
+        </div>
+      </div>
+
+      <div className="right-side">
+        {/* Content for the right side */}
+        <h1>Let's Get You Connected</h1>
+        <p>Lorem ipsum dolor sit amet consectetur. Aliquam pharetra vel tempus a ac in enim.</p>
+        <div className="buttons">
+          <button>Donor</button>
+          <button>Recipient</button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
